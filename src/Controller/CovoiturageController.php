5a9f61e0b670cc1d69f3e13d7cac526170c13cb2
clélia <clelia.panini.serres@live.Fr>
2025-05-09 11:43:11 +0200

    created listing covoiturage page with the controller to search a ride

--- src/Controller/CovoiturageController.php
+++ src/Controller/CovoiturageController.php
@@ -3,16 +3,41 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use \PDO;
 
-final class CovoiturageController extends AbstractController
+class CovoiturageController extends AbstractController
 {
     #[Route('/covoiturage', name: 'app_covoiturage')]
-    public function index(): Response
+    public function rechercher(Request $request): Response
     {
-        return $this->render('covoiturage/index.html.twig', [
+        $depart = $request->query->get('depart');
+        $arrivee = $request->query->get('arrivee');
+        $date = $request->query->get('date_depart');
+        $heure = $request->query->get('heure_depart');
+
+        //Connexion PDO manuelle
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=ecoride;charset=utf8', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //Requête préparée sécurisée
+        $stmt = $pdo->prepare("SELECT * FROM covoiturage WHERE lieu_depart = :depart AND lieu_arrivee = :arrivee AND date_depart = :date AND heure_depart >= :heure");
+
+        $stmt->execute([
+            ':depart' => $depart,
+            ':arrivee' => $arrivee,
+            ':date' => $date,
+            'heure' => $heure,
+        ]);
+
+        $trajets = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    
+        return $this->render('covoiturage/resultats.html.twig', [
             'controller_name' => 'CovoiturageController',
+            'trajets' => $trajets,
         ]);
     }
 }
